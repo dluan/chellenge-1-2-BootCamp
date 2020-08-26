4eb7a338aca7640c8f6d12f9eda8bd3faa3c7c20
Denner Luan <denner.gm@guiademoteis.com.br>
2020-08-26 12:31:09 +0100

    desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((acc, transaction) =>{
+      switch (transaction.type) {
+        case "income":
+          acc.income += transaction.value
+          break;
+        case "outcome":
+          acc.outcome += transaction.value
+          break;
+        default:
+          break;
+      }
+
+      acc.total = acc.income - acc.outcome;
+
+      return acc;
+    }, {
+      income:0,
+      outcome:0,
+      total: 0
+    });
+
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}:CreateDTO): Transaction {
+    const transaction = new Transaction({title, value, type})
+    this.transactions.push(transaction)
+    return transaction
   }
 }
 
